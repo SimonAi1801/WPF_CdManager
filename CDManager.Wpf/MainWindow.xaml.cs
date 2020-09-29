41bebe2ec348025c1dba797324e3ef3edaca4d74
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2020-09-29 20:03:32 +0200

    did improvements

--- CDManager.Wpf/MainWindow.xaml.cs
+++ CDManager.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CdManager.Model;
+using CDManager.Wpf.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,13 +27,55 @@ namespace CDManager.Wpf
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
+            btnNew.Click += BtnNew_Click;
+            btnDelete.Click += BtnDelete_Click;
+            btnEdit.Click += BtnEdit_Click;
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Cd selectedCd = listBoxCds.SelectedItem as Cd;
+
+            if (selectedCd == null)
+            {
+                MessageBox.Show("Sie müssen eine CD auswählen!");
+                return;
+            }
+            //Repo erweitern
+            Repository repo = Repository.GetInstance();
+            _cds = repo.GetAllCds();
+            listBoxCds.ItemsSource = _cds;
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Cd selectedCd = listBoxCds.SelectedItem as Cd;
+
+            if (selectedCd == null)
+            {
+                MessageBox.Show("Sie müssen eine CD auswählen!");
+                return;
+            }
+            //Repo erweitern
+            Repository repo = Repository.GetInstance();
+            _cds = repo.GetAllCds();
+            listBoxCds.ItemsSource = _cds;
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            AddCdWindow addCdWindow = new AddCdWindow();
+            addCdWindow.ShowDialog();
+
+            Repository repo = Repository.GetInstance();
+            _cds = repo.GetAllCds();
+            listBoxCds.ItemsSource = _cds;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             Repository repo = Repository.GetInstance();
             _cds = repo.GetAllCds();
-
             listBoxCds.ItemsSource = _cds;
         }
     }
---
+++ CDManager.Wpf/Windows/AddCdWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using CdManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CDManager.Wpf.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddCdWindow.xaml
+    /// </summary>
+    public partial class AddCdWindow : Window
+    {
+        public AddCdWindow()
+        {
+            InitializeComponent();
+            Loaded += AddCdWindow_Loaded;
+        }
+
+        private void BtnCancle_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Repository.GetInstance().AddCd(DataContext as Cd);
+            Close();
+        }
+
+        private void AddCdWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnSave.Click += BtnSave_Click;
+            btnCancle.Click += BtnCancle_Click;
+
+            DataContext = new Cd 
+            {
+                AlbumTitle = "Bitte geben Sie hier den Titel ein!",
+                Artist = "Bitte geben Sie hier den Artist ein"
+            };
+        }
+    }
+}
